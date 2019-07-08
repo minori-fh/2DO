fd35f84fb3e03b46b56bd49e426e573b1e6d1e3a
Minori Hashimoto <minori.fh@gmail.com>
2019-07-08 09:40:32 -0700

    add set timeout

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(timeout(15000));
+app.use(timeout(15000));
 // app.use(haltOnTimedout);
 
 // function haltOnTimedout(req, res, next) {
